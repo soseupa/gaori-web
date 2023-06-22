54bdb64aa8351c5d1476aa37493135686df4d540
kimhyerin0909 <khyerin0909@gmail.com>
2023-06-22 11:28:26 +0900

    Feat - route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Header } from "./components/shared/header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Main } from "./pages/main";
+import { Friends } from "./pages/friends";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/friends" element={<Friends />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
